049c5f02345334dffaf7fe2a7dfb1571267ce689
MR LUKE B DOOLAN <doolanl208@gmail.com>
2019-12-08 02:06:02 -0800

    more work on the video player

---
+++ Assets/BattlePhaze/BattlePhazeCore/BattlePhazeMeshCombine/BattlePhazeMeshCombine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Mesh Combine System
+/// </summary>
+public class BattlePhazeMeshCombine : MonoBehaviour
+{
+    public void MeshCombine(MeshFilter[] filters,MeshRenderer[] Renders)
+    {
+
+    }
+}
